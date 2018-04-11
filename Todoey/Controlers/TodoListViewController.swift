ef09c29e464ebaf3ff86df3686bd650fe2fbea47
Stephen Smith <workcpu@stephens-air>
2018-04-11 13:32:00 -0400

    Codeable protocol and plist

--- Todoey/Controlers/TodoListViewController.swift
+++ Todoey/Controlers/TodoListViewController.swift
@@ -12,30 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "EGGS"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "BREAD"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "TodoItemArray") as? [String] {
-           itemArray = items
-       }
     
+        loadItems()
     }
 
-    //TableView Data source methods
+//TableView Data source methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -49,12 +34,12 @@ class TodoListViewController: UITableViewController {
     
         //Ternary operator -- used instead of if, else
         
-        cell.accessoryType = tem.done ? .checkmark : .none
+        cell.accessoryType = item.done ? .checkmark : .none
 
         return cell
     }
     
-    //TableView Delegate Methods
+//TableView Delegate Methods
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
@@ -66,7 +51,7 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    // Mark - Add New Items
+// Mark - Add New Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -75,15 +60,14 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //what will happen once the user clicks the add item button on the UIAlert
+//what will happen once the user clicks the add item button on the UIAlert
             
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
+
+            self.saveItems()
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
         }
         
         alert.addTextField { (alertTextField) in
@@ -96,7 +80,35 @@ class TodoListViewController: UITableViewController {
 
         present (alert, animated: true, completion: nil)
     }
+    
+// Mark -- Model Manipulation Methods
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }catch{
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+        
+    }
+    
+    func loadItems() {
+        
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do{
+            itemArray = try decoder.decode([Item].self, from: data)
+        }catch{
+                print("Error decoding")
+            }
+            
+        }
+    
+    }
+
 }
-
-
 
