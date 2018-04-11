1d94a6aa5447bae57a6983e4b8a39b9e772e12d6
Stephen Smith <workcpu@stephens-air>
2018-04-11 10:42:27 -0400

    Convert to MVC design

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Stephen Smith on 4/10/18.
+//  Copyright © 2018 Stephen Smith. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title : String = ""
+    var done : Bool = false
+}
